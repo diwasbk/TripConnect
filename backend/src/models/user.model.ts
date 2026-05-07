1f52e31d1bf9fcbfa60866debe9ca8d0451d8925
diwasbk <diwasbk903@gmail.com>
2026-05-07 22:26:43 +0545

    feat(validation): add and refine package, itinerary, and departure schemas

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 import { signupType } from "../types/auth.types";
 
 const userSchema: Schema = new mongoose.Schema<signupType>({
---
+++ backend/src/types/package.types.ts
@@ -0,0 +1,74 @@
+import { z } from "zod";
+
+/* Departure Schema */
+export const departureSchema = z.object({
+    date: z
+        .coerce.date("Date is required."),
+    totalSeats: z
+        .number("Total seats is required.")
+        .min(1, "Total seats must be at least 1."),
+    availableSeats: z
+        .number("Available seats is required.")
+        .min(0, "Available seats cannot be negative.")
+});
+
+/* Itinerary Schema */
+export const itinerarySchema = z.object({
+    day: z
+        .number("Day is required."),
+    title: z
+        .string("Title is required.")
+        .nonempty("Title is required.")
+        .min(5, "Title must be at least 5 characters."),
+    description: z
+        .string("Description is required.")
+        .nonempty("Description is required.")
+        .min(20, "Description must be at least 20 characters."),
+    activities: z
+        .array(
+            z.string().min(2, "Activity must be at least 2 characters.")
+        )
+        .min(1, "At least one activity is required.")
+});
+
+/* Package Schema */
+export const packageSchema = z.object({
+    title: z
+        .string("Title is required")
+        .nonempty("Title is required")
+        .min(5, "Title must be at least 5 characters."),
+    intro: z
+        .string("Intro is required.")
+        .nonempty("Intro is required.")
+        .min(10, "Intro must be at least 10 characters."),
+    description: z
+        .string("Description is required.")
+        .nonempty("Description is required.")
+        .min(20, "Description must be at least 20 characters."),
+    duration: z
+        .string("Title is required.")
+        .nonempty("Title is required."),
+    price: z
+        .number("Title is required.")
+        .min(0, "Price cannot be negative."),
+    photoUrls: z
+        .array(
+            z.string().url("Invalid photo URL.")
+        )
+        .min(1, "At least one photo is required."),
+    includes: z
+        .array(
+            z.string().min(2, "Include item is too short.")
+        )
+        .min(1, "At least one include item is required."),
+    itinerary: z
+        .array(itinerarySchema)
+        .default([]),
+    departures: z
+        .array(departureSchema)
+        .default([]),
+    isActive: z
+        .boolean()
+        .default(true)
+});
+export type packageType = z.infer<typeof packageSchema>;
