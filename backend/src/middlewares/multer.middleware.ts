378afcc0f45eede9a022243498c8c170b01e3f55
diwasbk <diwasbk903@gmail.com>
2026-05-12 16:36:18 +0545

    feat(upload): add multer file upload middleware and static file serving

---
+++ backend/src/middlewares/multer.middleware.ts
@@ -0,0 +1,15 @@
+import { Request } from "express";
+import multer from "multer";
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+    destination: function (req: Request, file, cb) {
+        cb(null, "uploads");
+    },
+    filename: function (req: Request, file, cb) {
+        cb(null, Date.now() + "_" + file.originalname);
+    }
+});
+
+// Create and export multer upload middleware
+export const upload = multer({ storage });
