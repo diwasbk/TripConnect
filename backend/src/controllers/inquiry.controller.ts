5624a26bc89784c5b61425d5f41afcd9311cf617
diwasbk <diwasbk903@gmail.com>
2026-05-18 19:38:16 +0545

    feat(inquiry): add inquiry reply endpoint

--- backend/src/controllers/inquiry.controller.ts
+++ backend/src/controllers/inquiry.controller.ts
@@ -55,6 +55,42 @@ class InquiryController {
             });;
         };
     };
+
+    // Reply Inquiry 
+    replyInquiry = async (req: Request, res: Response) => {
+        try {
+            const { reply } = req.body;
+
+            const inquiryExist = await inquiryModel.findOneAndUpdate(
+                { _id: req.params.inquiryId },
+                { $set: { reply: reply, status: "replied" } },
+                { new: true }
+            );
+
+            if (!inquiryExist) {
+                return res.status(404).send({
+                    message: "Inquiry not found!",
+                    success: false
+                });
+            };
+
+            const html = generateInquiryReplyEmail(inquiryExist, reply);
+
+            await sendEmail(inquiryExist.email, "MediConnect - Response to Your Inquiry", html);
+
+            res.status(200).send({
+                message: "Reply sent successfully!",
+                success: true
+            });
+
+        } catch (err: any) {
+            console.log(err);
+            res.status(500).send({
+                message: err.message ? `Internal server error: ${err.message}` : "Internal server error.",
+                success: false
+            });;
+        };
+    };
 };
 
 export default InquiryController;
--- backend/src/routes/inquiry.route.ts
+++ backend/src/routes/inquiry.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import InquiryController from "../controllers/inquiry.controller";
 import schemaValidateMiddleware from "../middlewares/schema.validator.middleware";
-import { inquirySchema } from "../types/inquiry.types";
+import { inquiryReplySchema, inquirySchema } from "../types/inquiry.types";
 
 const inquiryRouter = express.Router();
 const inquiryController = new InquiryController();
 
 inquiryRouter.post("/send", schemaValidateMiddleware(inquirySchema), inquiryController.sendInquiry);
 inquiryRouter.get("/all", inquiryController.getAllInquiries);
+inquiryRouter.patch("/reply/:inquiryId", schemaValidateMiddleware(inquiryReplySchema), inquiryController.replyInquiry);
 
 export default inquiryRouter;
