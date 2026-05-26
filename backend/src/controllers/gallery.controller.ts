d1e050192f5f61dcdea9b23432eeb39c14817727
diwasbk <diwasbk903@gmail.com>
2026-05-26 14:14:28 +0545

    feat(gallery): add get gallery by ID

--- backend/src/controllers/gallery.controller.ts
+++ backend/src/controllers/gallery.controller.ts
@@ -27,5 +27,32 @@ class GalleryController {
             });
         };
     };
+
+    // Get Gallery By Gallery ID
+    getGalleryByGalleryId = async (req: Request, res: Response) => {
+        try {
+            const galleryExist = await GalleryModel.findOne({ _id: req.params.galleryId });
+
+            if (!galleryExist) {
+                return res.status(404).send({
+                    message: "Gallery not found!",
+                    success: false
+                });
+            };
+
+            res.status(200).send({
+                message: "Gallery fetched successfully!",
+                result: galleryExist,
+                success: true
+            });
+
+        } catch (err: any) {
+            console.log(err);
+            res.status(500).send({
+                message: err.message ? `Internal server error: ${err.message}` : "Internal server error.",
+                success: false
+            });
+        };
+    };
 };
 export default GalleryController;
--- backend/src/routes/gallery.route.ts
+++ backend/src/routes/gallery.route.ts
@@ -2,10 +2,12 @@ import express from "express";
 import GalleryController from "../controllers/gallery.controller";
 import schemaValidateMiddleware from "../middlewares/schema.validator.middleware";
 import { gallerySchema } from "../types/gallery.types";
+import { upload } from "../middlewares/multer.middleware";
 
 const galleryRouter = express.Router();
 const galleryController = new GalleryController();
 
 galleryRouter.post("/create", schemaValidateMiddleware(gallerySchema.pick({ title: true, caption: true })), galleryController.createGallery);
+galleryRouter.get("/:galleryId", galleryController.getGalleryByGalleryId);
 
 export default galleryRouter;
