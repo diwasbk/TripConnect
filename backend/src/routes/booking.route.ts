177759934be893169b483937aacce90de9d31a5b
diwasbk <diwasbk903@gmail.com>
2026-06-08 22:27:16 +0545

    refactor(booking): handle guest and logged-in users with a single booking API

--- backend/src/routes/booking.route.ts
+++ backend/src/routes/booking.route.ts
@@ -2,13 +2,12 @@ import express from "express";
 import BookingController from "../controllers/booking.controller";
 import schemaValidateMiddleware from "../middlewares/schema.validator.middleware";
 import { bookingSchema } from "../types/booking.types";
-import { jwtAuthMiddleware } from "../utils/jwt";
+import { optionalAuthMiddleware } from "../middlewares/auth.middleware";
 
 const bookingRouter = express.Router();
 const bookingController = new BookingController();
 
-bookingRouter.post("/guest-user/:packageId", schemaValidateMiddleware(bookingSchema), bookingController.bookPackageForGuestUserByPackageId);
-bookingRouter.post("/registered-user/:packageId", jwtAuthMiddleware, schemaValidateMiddleware(bookingSchema.pick({ travelDate: true, noOfTravellers: true })), bookingController.bookPackageForRegisteredUserByPackageId);
+bookingRouter.post("/:packageId", optionalAuthMiddleware, schemaValidateMiddleware(bookingSchema), bookingController.bookPackageByPackageId);
 bookingRouter.get("/all", bookingController.getAllBookingsByStatus);
 bookingRouter.get("/booking-id/:bookingId", bookingController.getBookingByBookingId);
 bookingRouter.get("/booking-reference/:bookingReference", bookingController.getBookingByBookingReference);
