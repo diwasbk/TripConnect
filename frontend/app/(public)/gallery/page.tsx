cb08923dfa2a26b3aad7b2f6756f761593485f35
diwasbk <diwasbk903@gmail.com>
2026-06-10 18:18:14 +0545

    feat(gallery): add gallery page and gallery section component

---
+++ frontend/app/(public)/gallery/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "../_components/footer";
+import NavBar from "../_components/navbar";
+import GallerySection from "./_components/gallery-section";
+
+export default function HomePage() {
+    return (
+         <div className="relative bg-linear-to-br from-emerald-50 via-white to-lime-50">
+            <NavBar/>
+            <GallerySection/>
+            <Footer/>
+        </div>
+    );
+}
