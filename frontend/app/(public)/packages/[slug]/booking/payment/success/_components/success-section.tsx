064ea7fd6aeb4516d4140f409f2db74103a6dd56
diwasbk <diwasbk903@gmail.com>
2026-06-29 16:54:55 +0545

    feat(frontend): verify eSewa payment on success callback

--- frontend/app/(public)/packages/[slug]/booking/payment/success/_components/success-section.tsx
+++ frontend/app/(public)/packages/[slug]/booking/payment/success/_components/success-section.tsx
@@ -1,13 +1,37 @@
 "use client";
+import { handleVerifyEsewaPayment } from "@/lib/actions/payment-action";
 import Link from "next/link";
 import { useParams, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 import { FiCheck } from "react-icons/fi";
+import { toast } from "react-toastify";
 
 export default function SuccessSection({ navUrl1, navUrl2 }: { navUrl1: string, navUrl2: string }) {
     const params = useParams();
     const packageSlug = params?.slug as string;
+    const bookingReference = params?.bookingReference as string;
+
     const searchParams = useSearchParams();
-    const bookingReference = searchParams.get("bookingReference") as string;
+    const data = searchParams.get("data") as string;
+
+    useEffect(() => {
+        const verifyEsewaPayment = async () => {
+            try {
+                if (!data) return;
+
+                const res = await handleVerifyEsewaPayment(data);
+
+                if (!res.success) {
+                    throw new Error(res.message || "Failed to verify payment!");
+                }
+            } catch (err: any) {
+                toast.error(err.message || "Failed to verify payment!");
+            }
+        };
+
+        verifyEsewaPayment();
+    }, []);
+
     return (
         <div className="mx-auto w-full max-w-2xl px-4 py-12">
             <div className="rounded-4xl border border-emerald-100 bg-white p-8 text-center shadow-xl">
